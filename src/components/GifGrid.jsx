816adecac46ffb1f126678968f340c049dd05ef5
J. Carlos Guevara <jguevara@exelco.mx>
2022-12-20 13:04:23 -0600

    gifs titles

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -2,14 +2,24 @@ import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  
+
+  const [images, setImages] = useState([]);
+
     useEffect( () => {
-      getGifs(category);
+      getGifs(category)
+        .then( newImages => setImages(newImages) );
     }, [])
 
   return (
     <>
         <h3>{category}</h3>
+        <ol>
+            {
+              images.map(image => (
+                <li key={image.id}>{image.title}</li>
+              ))
+            }
+        </ol>
 
     </>
   )
